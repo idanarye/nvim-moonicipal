2ce230888fc9931d2132854dab3604d9b303350e
Idan Arye <IdanArye@gmail.com>
2022-11-02 14:56:17 +0200

    Remove argument parsing
    May be added later, when Moonicipal is more mature, or maybe as a third
    party library.

--- lua/moonicipal/regular_task.lua
+++ lua/moonicipal/regular_task.lua
@@ -1,13 +1,9 @@
-local args_context = require'moonicipal/args_context'
-
 local RegularTask = setmetatable({}, require'moonicipal/task_type_meta')
 
 function RegularTask:run(context, task_def, args)
-    local task_instance = {}
-    if task_def.args then
-        local extracted = args_context.extract(task_def.args, {unpack(args or {})})
-        task_instance = vim.tbl_extend('error', task_instance, extracted)
-    end
+    local task_instance = {
+        args = args;
+    }
     task_def.run(task_instance)
 end
 
