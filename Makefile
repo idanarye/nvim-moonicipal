a366ee9b85158f1ac3b45e14c4508601dfc97899
Idan Arye <IdanArye@gmail.com>
2022-12-25 16:58:37 +0200

    Start writing the tests

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: docs test
+
+test:
+	nvim --headless -u tests/minimal_init.lua -c "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minimal_init.lua'}"
+
+
+# docs:
+	# mkdir -p doc
+	# lemmy-help --prefix-func lua/moonicipal/{init,}.lua | tee doc/moonicipal.txt
---
+++ tests/minimal_init.lua
@@ -0,0 +1,59 @@
+vim.o.shada = ''
+
+vim.opt.runtimepath:append { '.' }
+vim.opt.runtimepath:append { '../plenary.nvim' }
+
+require'moonicipal'.setup {
+    file_prefix = '.testing',
+}
+
+local MOONICIPAL_TASKS_FILE_NAME = '.testing.moonicipal.lua'
+
+function BeforeTestCleanup()
+    OUTPUT = nil
+    -- Ensure single window
+    local wins = vim.api.nvim_list_wins()
+    vim.cmd.new()
+    for _, winnr in ipairs(wins) do
+        vim.api.nvim_win_close(winnr, true)
+    end
+    -- Remove tasks file
+    vim.fn.delete(MOONICIPAL_TASKS_FILE_NAME)
+end
+
+function SetTasksFile(content)
+    vim.fn.writefile(vim.split(content, '\n'), MOONICIPAL_TASKS_FILE_NAME)
+end
+
+function SetTasksFileTasks(content)
+    SetTasksFile(table.concat({
+        'local moonicipal = require"moonicipal"',
+        'local T = moonicipal.tasks_file()',
+        '',
+        content,
+    }, '\n'))
+end
+
+function Sleep(duration)
+    local co = coroutine.running()
+    vim.defer_fn(function()
+        coroutine.resume(co)
+    end, duration)
+    coroutine.yield()
+end
+
+function WaitFor(timeout_secs, pred, sleep_ms)
+    local init_time = vim.loop.uptime()
+    local last_time = init_time + timeout_secs
+    while true do
+        local iteration_time = vim.loop.uptime()
+        local result = {pred()}
+        if result[1] then
+            return unpack(result)
+        end
+        if last_time < iteration_time then
+            error('Took too long (' .. (iteration_time - init_time) .. ' seconds)')
+        end
+        Sleep(sleep_ms or 10)
+    end
+end
